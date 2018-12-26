4d01d912635666eff2960586e66eb47aedd93d56
jinzvip <37496016+jinzvip@users.noreply.github.com>
2018-12-26 14:20:36 +0800

    Update blog.js

--- project1/js/blog.js
+++ project1/js/blog.js
@@ -5,23 +5,23 @@ function showTime(){
 		case 12:
 		case 13:
 		case 14:
-			document.getElementById("time").innerHTML="中午好！欢迎来到海软博客";
+			document.getElementById("time").innerHTML="中午好！欢迎来到Jinz博客";
 		break;
 		case 15:
 		case 16:
 		case 17:
 		case 18:
-			document.getElementById("time").innerHTML="下午好！欢迎来到海软博客";
+			document.getElementById("time").innerHTML="下午好！欢迎来到Jinz博客";
 		break;
 		case 19:
 		case 20:
 		case 21:
 		case 22:
 		case 23:
-			document.getElementById("time").innerHTML="晚上好！欢迎来到海软博客";
+			document.getElementById("time").innerHTML="晚上好！欢迎来到Jinz博客";
 		break;
 		default:
-		document.getElementById("time").innerHTML="上午好！欢迎来到海软博客";
+		document.getElementById("time").innerHTML="上午好！欢迎来到Jinz博客";
 	}
 }
 window.onload=showTime;
